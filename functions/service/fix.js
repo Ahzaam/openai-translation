96fe2f3e6f3675568fc33b3c38fe18ef31422c7f
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-03-28 21:55:04 +0530

    caption

--- functions/service/fix.js
+++ functions/service/fix.js
@@ -1,3 +1,17 @@
-exports.fixEveryThing = async (prompt) => {
-  console.log(prompt);
+const openai = require("./openaiconfig");
+
+exports.fixEveryThing = (prompt) => {
+  return new Promise(async (resolve, reject) => {
+    const request = {
+      model: "gpt-3.5-turbo",
+      max_tokens: 2048,
+      messages: [{ role: "user", prompt }],
+
+      temperature: 0.8,
+    };
+    console.log(prompt);
+
+    const response = await openai.createEdit(request);
+    resolve(response.data.choices[0].text);
+  });
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,39 +1,103 @@
-import { SendSVG } from "../components/icons";
-
+import { SendSVG, CopySVG } from "../components/icons";
+import { functions } from "../service/firebase";
+import { useState } from "react";
+import "../App.css";
 export default function HomePage() {
-  return (
-    <div className=" p-4 rounded-lg   max-w-4xl mx-auto ">
-      <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full pb-9 px-5  max-w-4xl">
-        <div className="flex items-center rounded-lg border bg-white border-gray-400 pl-3 pr-1  py-1">
-          <textarea
-            type="text"
-            placeholder="Type a prompt"
-            className="w-full focus:outline-none"
-            cols="30"
-            rows="10"
-          ></textarea>
+  const [results, setResult] = useState([]);
+  const [prompt, setPrompt] = useState("");
 
-          <button className="bg-blue-600 flex items-center justify-center font-bold py-1 h-9 w-14 text-center px-4 ml-2 rounded">
-            {" "}
-            <SendSVG />
-          </button>
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setPrompt(e.target.value);
+  };
+
+  const handleFix = () => {
+    functions
+      .httpsCallable("fix")({
+        prompt: prompt,
+      })
+      .then((response) => {
+        setResult([...results, response.data]);
+        console.log(response.data);
+      });
+  };
+
+  return (
+    <div className="bg-gray-50" style={{ minHeight: "90vh" }}>
+      <div className=" p-4 rounded-lg   max-w-4xl mx-auto ">
+        <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full pb-9 px-5  max-w-4xl">
+          <div className="flex items-center rounded-lg border bg-white border-gray-400 pl-3 pr-1  py-1">
+            <textarea
+              onChange={handleChange}
+              type="text"
+              placeholder="Type a prompt"
+              className="w-full focus:outline-none"
+              cols="30"
+              rows="5"
+            ></textarea>
+
+            <button
+              onClick={handleFix}
+              className="bg-blue-600 flex items-center justify-center font-bold py-1 h-9 w-14 text-center px-4 ml-2 rounded"
+            >
+              {" "}
+              <SendSVG />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className=" p-4 rounded-lg text-center my-4 flex flex-col min-h-full bg-white">
-        <h2 className="font-bold mb-2 text-2xl">
-          AI Social Media Caption Writing
-        </h2>
-        <p className="text-gray-700 mb-4">
-          Our website's AI feature can help you write captivating and effective
-          captions for your social media posts. Say goodbye to writer's block
-          and hello to more engagement!
-        </p>
-      </div>
-      <div
-        className=" p-4 rounded-lg text-center flex flex-col min-h-full bg-white"
-        style={{ minHeight: "68vh" }}
-      >
-        {/*  Output Display */}
+        <div
+          className=" p-4 rounded-lg text-center my-4 flex flex-col min-h-full bg-white mb-56"
+          style={{ minHeight: "70vh" }}
+        >
+          <h2 className="font-bold mb-2 text-2xl">
+            AI Social Media Caption Writing
+          </h2>
+          {results.map((result, ind) => {
+            return (
+              <div
+                className="bg-gray-50 rounded-lg shadow-md p-4 mb-3 fade-in"
+                key={ind}
+              >
+                <div className="flex items-center justify-between bg-gray-100 rounded-md p-3">
+                  <p
+                    id={"captionID-" + ind}
+                    className="text-gray-700"
+                    aria-label="copy-text"
+                  >
+                    {result}
+                  </p>
+                </div>
+                {/* COPY BUTTON */}
+                <div
+                  className="text-right mt-2"
+                  onClick={(e) => {
+                    var text = document.querySelector(
+                      "#captionID-" + ind
+                    ).textContent;
+                    navigator.clipboard.writeText(text);
+                    document.querySelector("#copybtn-" + ind).textContent =
+                      "Copied!";
+                    setTimeout(() => {
+                      document.querySelector("#copybtn-" + ind).textContent =
+                        "Copy";
+                    }, 2000);
+                  }}
+                >
+                  <div className="ml-auto w-fit flex bg-gray-100 py-1 px-3 rounded-lg cursor-pointer">
+                    <div id={"copybtn-" + ind} className="pb-2 mx-2">
+                      Copy
+                    </div>{" "}
+                    <div className=" mt-1  ">
+                      <CopySVG />
+                    </div>
+                  </div>
+                </div>
+
+                {/* --------------- */}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
